69cfba248b8859060cf9065a04f7beaa95f065ec
Nataniel <nataniel.o@lyncas.net>
2024-06-26 00:02:16 -0300

    envio de email novo comentario

---
+++ src/email/email.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EmailService {
+  private transporter: nodemailer.Transporter;
+
+  constructor(private configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      host: this.configService.get<string>('EMAIL_HOST'),
+      port: this.configService.get<number>('EMAIL_PORT'),
+      auth: {
+        user: this.configService.get<string>('EMAIL_USER'),
+        pass: this.configService.get<string>('EMAIL_PASS'),
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, text: string) {
+    const mailOptions = {
+      from: this.configService.get<string>('EMAIL_FROM'),
+      to,
+      subject,
+      text,
+    };
+
+    return await this.transporter.sendMail(mailOptions);
+  }
+}
